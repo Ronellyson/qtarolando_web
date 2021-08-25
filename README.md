ce3c74b69809ea1f5a69111f8b3659dd7db2dc15
Ronellyson <ronellysonjulio@gmail.com>
2021-08-25 19:21:12 -0300

    build web do aplicativo qtarolando

---
+++ README.md
@@ -0,0 +1,69 @@
+# QTáRolando_web?
+
+Esta é uma versão web do app qtarolando desenvolvido, em Flutter e Dart, para o projeto de Extensão Apps4Society do Campus IV - UFPB, em Rio Tinto, cujo o objetivo é oferecer, de forma simples e fácil, tanto a visualização quanto as informações de eventos, inicialmente com o foco aos relacionados a UFPB, no estado da Paraíba.
+
+### QTáRolando? Screenshots
+
+<table>
+  <tr>
+    <td>Homepage</td>
+    <td>Search</td>
+    <td>Filter</td>
+  </tr>
+  <tr>
+    <td><img src="screenshots/Homepage-Light.png" width=270 height=540></td>
+    <td><img src="screenshots/Search.png" width=270 height=540></td>
+    <td><img src="screenshots/Filter.png" width=270 height=540></td>
+  </tr>
+  <tr>
+    <td>Share</td>
+    <td>Event Details</td>
+    <td>Dark Theme</td>
+  </tr>
+  <tr>
+    <td><img src="screenshots/Share.png" width=270 height=540></td>
+    <td><img src="screenshots/Event-Details.png" width=270 height=540></td>
+    <td><img src="screenshots/HomePage-Dark.png" width=270 height=540></td>
+  </tr>
+ </table>
+
+## Pré Requisitos
+### Flutter v1.22.2 ou superior
+Como iniciar com o Flutter: https://flutter.dev/docs/get-started/install
+### Hasura Engine
+Como iniciar com o Docker: [https://hasura.io/docs/1.0/graphql/manual/getting-started/docker-simple.html#docker-simple](https://hasura.io/docs/1.0/graphql/manual/getting-started/docker-simple.html#docker-simple)
+
+Como iniciar com o Heroku: [https://hasura.io/docs/cloud/1.0/manual/getting-started/index.html](https://hasura.io/docs/cloud/1.0/manual/getting-started/index.html)
+
+O **Hasura**, em seu arquivo docker-compose, ao se iniciar com o Docker, exige o **PostgreSQL** em sua versão 12 e utiliza uma imagem do **GraphQL**, do próprio Hasura, em sua versão 1.3.0.
+
+### Plugins/Extensões
+Embora os plugins, que serão listados abaixo, não sejam necessários para a execução do projeto, auxiliam muito na produtividade durante o desenvolvimento do mesmo.
+
+OBS: Tanto os plugins quanto o projeto podem ser encontrados/desenvolvido no Android Studio ou no Visual Studio Code.
+
+#### Dart
+O Dart  fornece ao VS Code ou Android Studio, suporte para a linguagem de programação Dart e  ferramentas para editar, refatorar, executar e recarregar aplicativos móveis Flutter e aplicativos web AngularDart com eficácia.
+#### Flutter
+Esta extensão do VS Code ou Android Studio adiciona suporte para edição, refatoração, execução e recarga de aplicativos móveis Flutter de maneira eficaz, bem como suporte para a linguagem de programação Dart.
+
+## Iniciando o Projeto
+Após baixar/clonar o projeto abra a pasta do projeto em seu ambiente de desenvolvimento (Android Studio ou no Visual Studio Code), no console ou terminal do ambiente execute os seguintes comandos:
+
+### Comando para baixar todas as dependências do projeto:
+```{sh}
+flutter pub get
+```
+#### Observação
+Antes de iniciar o aplicativo, certifique-se de ter colocado o endereço do Hasura GraphQL Endpoint no arquivo app_module, que está localizado na pasta: lib/app/app_module.dart.
+Dentro do arquivo, procure o trecho de código:
+```{sh}
+Dependency((i) => HasuraConnect("address of your Hasura GraphQL Endpoint"))
+```
+Coloque o endereço entre aspas, e pronto!
+
+### Com um dispositivo conectado ou emulador aberto, execute o seguinte comando:
+```{sh}
+flutter run
+```
+Pronto o projeto já estará executando, let's code!.
---
+++ flutter_service_worker.js
@@ -0,0 +1,212 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "assets/AssetManifest.json": "8a700ea838832bd52ff1a46f7d4c8ab8",
+"assets/assets/fonts/cretype%2520%2520Caros%2520Bold.otf": "e6ff9a2c6530276522d4eb4868ed4197",
+"assets/assets/fonts/cretype%2520%2520Caros%2520ExtraBold.otf": "03ff0d35c12a9d155850bfcb08b49b86",
+"assets/assets/fonts/cretype%2520%2520Caros%2520Light.otf": "7235a07b741405b836b33fae6e3738bd",
+"assets/assets/fonts/cretype%2520%2520Caros%2520Medium.otf": "0f9e62140e2c6faad7ac001ed9bdcbce",
+"assets/assets/fonts/cretype%2520%2520Caros%2520Thin.otf": "720930c16bb3cfdf0d0805f34f2bbf68",
+"assets/assets/fonts/cretype%2520%2520Caros.otf": "427ad353f39abf0124242c7431d3b8e4",
+"assets/assets/icons/app_icon.png": "52370976f2ac6268f2e61db3c151e5d0",
+"assets/assets/icons/map_icon.png": "6ac3b4c772f32be260e8da5a7e985a0b",
+"assets/assets/icons/moon.svg": "76ab4762ee365b8deee6e62afe0001d4",
+"assets/assets/icons/no_wifi.svg": "f653a3520f10cb48952457d60780287b",
+"assets/assets/icons/preferences.svg": "f3ca86a340b4fd00249de82a257daf1c",
+"assets/assets/icons/sun.svg": "06c5b7a547874fe85b8b49baea60122c",
+"assets/assets/icons/times-circle-regular.svg": "3b2678c02d90aaa0d2ae380a119c234b",
+"assets/assets/images/imageNotFound.jpg": "c0d07da95355c91009204c9bb2cb932b",
+"assets/assets/images/logo.png": "203904a20655e29ba830b039e83d3099",
+"assets/assets/images/placeholder.png": "bf637d16cb0af9b7b603b53cd4a3cf3d",
+"assets/assets/images/sky_background.png": "9f2551842dc642ae89e482fb0f461a4f",
+"assets/assets/images/sky_background.svg": "640f6b72eb43f7df46c8c8be36c094b3",
+"assets/FontManifest.json": "8d7110eca6921c48243b5fba431d06b8",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/NOTICES": "bed03613583826cd6e5f4af8c4d17447",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"assets/packages/eva_icons_flutter/lib/fonts/evaicons.ttf": "b600c99b39c9837f405131463e91f61a",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "3241d1d9c15448a4da96df05f3292ffe",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "eaed33dc9678381a55cb5c13edaf241d",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "ffed6899ceb84c60a1efa51c809a57e4",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"index.html": "992effa00af621aef371d17a73295523",
+"/": "992effa00af621aef371d17a73295523",
+"main.dart.js": "a5964ef6cb8fa41705b952c37e5f1bc9",
+"manifest.json": "fa82fe879649099fb0c8d176edadf56b",
+"version.json": "7f9ac1090b53d7dd62ad55f05686e39e"
+};
+
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = [
+  "/",
+"main.dart.js",
+"index.html",
+"assets/NOTICES",
+"assets/AssetManifest.json",
+"assets/FontManifest.json"];
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache.
+        return response || fetch(event.request).then((response) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+    })
+  );
+});
+
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
